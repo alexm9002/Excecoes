f8d14891a7de617c59a3b815103b7757efe4cd65
Alex Marcelo <alexm9002@gmail.com>
2022-01-25 12:37:21 -0300

    Exercício Exceções

---
+++ Excecoes/Entidades/Reservas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Excecoes.Entidades {
+    internal class Reservas {
+        public int NumeroQuarto { get; set; }
+        public DateTime Entrada { get; set; }
+        public DateTime Saida { get; set; }
+
+        public Reservas(int numeroQuarto, DateTime entrada, DateTime saida) {
+            this.NumeroQuarto = numeroQuarto;
+            this.Entrada = entrada;
+            this.Saida = saida;
+        }
+        public int Duracao() {
+            // TimeSpan é usado para calcular duração de um período
+            TimeSpan duracao = Saida.Subtract(Entrada);
+
+            // Para transformar em dias usa-se a função "Total.Days"
+            // transformando os dados em inteiro, pois o retorno do
+            // "TotalDays" é feito em formato "Double".
+            return (int)duracao.TotalDays;
+        }
+        public void AtualizarData(DateTime entrada, DateTime saida) {
+            this.Entrada = entrada;
+            this.Saida = saida;
+        }
+
+        public override string ToString() {
+            return "Quarto: "
+            + NumeroQuarto
+            + ", Check-in: "
+            + Entrada.ToString("dd/MM/yyyy")
+            + ", Check-out: "
+            + Saida.ToString("dd/MM/yyyy")
+            + ", "
+            + Duracao() //Colocar a função e não a variável.
+            + " Noites.";
+        }
+    }
+}
---
+++ Excecoes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Excecoes.Entidades;
+
+
+namespace Excecoes {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Número do Quarto: ");
+            int numeroQuarto = int.Parse(Console.ReadLine());
+            Console.Write("Data do Check-in (dd/MM/yyyy): ");
+            DateTime entrada = DateTime.Parse(Console.ReadLine());
+            Console.Write("Data do Check-out (dd/MM/yyyy):");
+            DateTime saida = DateTime.Parse(Console.ReadLine());
+
+            if (saida <= entrada) {
+                Console.WriteLine("Erro na reserva! Data de Check-out não pode ser anterior a Data de Check-in ");
+            }
+            else {
+                Reservas reservas = new Reservas(numeroQuarto, entrada, saida);
+                Console.WriteLine("Reserva --> " + reservas);
+                Console.WriteLine();
+                Console.WriteLine("Entre com os dados para atualizar a reserva!");
+                Console.Write("Data do Check-in (dd/MM/yyyy): ");
+                entrada = DateTime.Parse(Console.ReadLine());
+                Console.Write("Data do Check-out (dd/MM/yyyy):");
+                saida = DateTime.Parse(Console.ReadLine());
+
+                DateTime hoje = DateTime.Now;
+                if (entrada < hoje || saida < hoje) {
+                    Console.WriteLine("Erro na reserva! A data lançada para reserva precisa ser futura");
+                }
+                else if (saida <= entrada) {
+                    Console.WriteLine("Erro na reserva! Data de Check-out não pode ser anterior a Data de Check-in ");
+                }
+                else {
+                    reservas.AtualizarData(entrada, saida);
+                    Console.WriteLine("Reservas: " + reservas);
+                }
+            }
+        }
+    }
+}
